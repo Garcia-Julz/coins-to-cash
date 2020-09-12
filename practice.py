ebcf0df8072825e0077e1c3b8dbacc1e80a450cf
Julian Garcia <garcia.j.julian@gmail.com>
2020-09-12 13:46:50 -0500

    restored

---
+++ practice.py
@@ -0,0 +1,29 @@
+# Python3 code to demonstrate  
+# rotation of list  
+# using slice  
+  
+# initializing list 
+test_list = [1, 4, 6, 7, 2, 11, 44, 66, 77, 22] 
+  
+# printing original list  
+print ("Original list : " + str(test_list)) 
+  
+# using slicing to left rotate by 3 
+test_list = test_list[3:] + test_list[:3] 
+  
+# Printing list after left rotate 
+print ("List after left rotate from [3] in list : " + str(test_list)) 
+  
+# using slicing to right rotate by 3 
+# back to Original 
+test_list = test_list[-3:] + test_list[:-3] 
+  
+# Printing after right rotate 
+print ("List after right rotate by 3(back to original) : "
+                                         + str(test_list))
+
+
+test_list = test_list[1-5]
+print('REVERSE' + str(test_list))
+                                        
+
---
+++ pythonFunctions.py
@@ -0,0 +1,30 @@
+
+
+def make_family(name, **member):
+    family_stuff = member.items()
+    family_str = f"We are the {name} family. "
+    for title, person in family_stuff:
+        family_str += f"The {title} is {person}. "
+    return family_str
+
+
+the_shepherds = make_family("Shepherd", mom="Anne", dad="Joe", dog="Murph")
+the_parkers = make_family("Parker", aunt="May", nephew="Peter")
+print(the_shepherds)
+print(the_parkers)
+
+
+def calculate_average(subject, *test_scores, **student_details):
+    # print("Type & value of subject: ", type(subject), subject)
+    # print("Type & value of test_scores: ", type(test_scores), test_scores)
+    # print("Type & value of the student_details: ", type(student_details), student_details)
+
+    if subject == "English":
+        average_score = 49
+    else:
+        average_score = sum(test_scores)/len(test_scores)
+    
+    print(f"{student_details['last_name']}, {student_details['first_name']} got an average score of {average_score} in {subject}.")
+
+
+calculate_average("Math", 75, 64, 35, first_name="Bryan", last_name="Nilsen")
