3b913885316fdbdd9dffe1f49e3e5d3f833bb102
Julian Garcia <garcia.j.julian@gmail.com>
2020-10-06 23:32:43 -0500

    added coins to cash function

---
+++ coinsToCash.py
@@ -0,0 +1,14 @@
+
+
+
+def calc_dollars(**coins):
+    # The function should look at each key (pennies, nickels, dimes and quarters) and perform the appropriate math on the integer value to determine how much money you have in dollars. Store that value in a variable named `dollarAmount` and print it.
+    p = .01
+    n = .05
+    d = .10
+    q = .25
+    
+
+calc_dollars(pennies= 342, nickels=9, dimes=32, quarters=4)
+
+# The output would be 8.07 when the function is executed with those arguments.
